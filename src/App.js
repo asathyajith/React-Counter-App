47a0b86a9374d2cc6bb30ec0eb34884312fdc18c
asathyjith <asathyajith98@gmail.com>
2022-06-23 22:51:42 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import FirstComp from "./components/first";
+import SecondComp from "./components/second";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<FirstComp/>}></Route>
+        <Route path="/second" element={<SecondComp/>} exact></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/second.jsx
@@ -0,0 +1,15 @@
+import { React } from "react";
+import "../App.css";
+
+function FirstComp() {
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello counter</p>
+      </header>
+    </div>
+  );
+}
+
+export default FirstComp;
